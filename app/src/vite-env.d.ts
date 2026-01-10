fd28a6d361b83929d5d4b61d5efa7425b4b99a1a
Umbraw <umbraw.devs@gmail.com>
2026-01-10 09:09:08 +0100

    small fix

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+export {}
+
+declare global {
+  interface Window {
+    loadgic: {
+      minimize: () => void
+      close: () => void
+      toggleFullscreen: () => void
+    }
+  }
+}
