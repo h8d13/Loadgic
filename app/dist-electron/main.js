b16771386f2178ed85eabc3117ff42e4aa231681
h8d13 <hadean-eon-dev@proton.me>
2026-01-13 17:49:59 +0100

    Add wrapper

--- app/dist-electron/main.js
+++ app/dist-electron/main.js
@@ -23,7 +23,7 @@ if (process.platform === "linux") {
     app.commandLine.appendSwitch("disable-features", "WaylandWpColorManagerV1");
     app.commandLine.appendSwitch("ozone-platform", "wayland");
     app.commandLine.appendSwitch("enable-features", "UseOzonePlatform");
-  } else if (ds_type === "x11") {
+  } else if (ds_type === "x11" || !ds_type) {
     app.commandLine.appendSwitch("ozone-platform", "x11");
   }
 }
